a45736d8360fa3170fd83301e7695681d7cffa43
ivan koop <ivankoop3@gmail.com>
2018-07-14 19:35:27 -0400

    files organized

--- module1-project/src/index.js
+++ module1-project/src/index.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+import App from "./components/App.jsx";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
