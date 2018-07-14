4538967f3999b7650470936a034d043a59b65887
ivan koop <ivankoop3@gmail.com>
2018-07-14 19:31:05 -0400

    Initial commit

---
+++ module1-project/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
