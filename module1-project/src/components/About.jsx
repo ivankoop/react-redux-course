0cfc739c3c215182d8b6076ce4de8c0a9c35c2d0
ivan koop <ivankoop3@gmail.com>
2018-07-14 19:49:45 -0400

    some components

---
+++ module1-project/src/components/About.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About = () => {
+  return <h2>My name is Ivan Koop and I am a React Developer.</h2>;
+};
+
+export default About;
--- module1-project/src/components/App.jsx
+++ module1-project/src/components/App.jsx
@@ -1,11 +1,23 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Switch, Redirect, Route } from "react-router-dom";
+import NavBar from "./NavBar.jsx";
+import About from "./About.jsx";
+import Resume from "./Resume.jsx";
+import Error from "./Error.jsx";
 
 const App = () => {
   return (
     <BrowserRouter>
       <div>
-        <h1>Hello World</h1>
+        <NavBar />
+        <Switch>
+          <Redirect exact path="/" to="/about" />
+          <Route path="/about" component={About} />
+          <Route path="/resume" component={Resume} />
+          <Route path="/projects" component={null} />
+          <Route path="/contact" component={null} />
+          <Route component={Error} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
