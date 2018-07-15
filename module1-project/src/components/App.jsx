453423ed62de7d5a38ed925a5648498f04f226c8
ivan koop <ivankoop3@gmail.com>
2018-07-14 20:03:36 -0400

    contact and projects components

--- module1-project/src/components/App.jsx
+++ module1-project/src/components/App.jsx
@@ -4,6 +4,8 @@ import NavBar from "./NavBar.jsx";
 import About from "./About.jsx";
 import Resume from "./Resume.jsx";
 import Error from "./Error.jsx";
+import Projects from "./Projects.jsx";
+import Contact from "./Contact.jsx";
 
 const App = () => {
   return (
@@ -14,8 +16,8 @@ const App = () => {
           <Redirect exact path="/" to="/about" />
           <Route path="/about" component={About} />
           <Route path="/resume" component={Resume} />
-          <Route path="/projects" component={null} />
-          <Route path="/contact" component={null} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/contact" component={Contact} />
           <Route component={Error} />
         </Switch>
       </div>
